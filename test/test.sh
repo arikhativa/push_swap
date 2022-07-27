29a41baeeb66b6a18d0335882ab34b9f19e450f6
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-07-27 18:37:24 +0200

    need optimization haha

--- test/test.sh
+++ test/test.sh
@@ -5,7 +5,9 @@ CHECKER=checker_Mac
 
 RES_DIR=./test/resource
 
-FILES=($RES_DIR/one_num_no_action $RES_DIR/two_num_no_action $RES_DIR/10 $RES_DIR/7 $RES_DIR/many)
+FILES=($RES_DIR/one_num_no_action $RES_DIR/two_num_no_action $RES_DIR/10 $RES_DIR/7 $RES_DIR/100 $RES_DIR/500 $RES_DIR/5)
+
+# add test for 5 buy 12
 
 i=0
 while [ $i -ne ${#FILES[@]} ]
